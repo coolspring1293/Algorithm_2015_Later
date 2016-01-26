7be91b0711d430245c52f351f339864853896f90
coolspring1293 <coolspring1293@outlook.com>
2016-01-26 15:45:09 +0800

    期末

---
+++ Review/Happy New Year/Happy New Year/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Happy New Year
+//
+//  Created by Kieran Will on 1/1/16.
+//  Copyright © 2016 Kieran Will. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Happy New Year, 2016!\n";
+    return 0;
+}
---
+++ Review/Problem 1. A+B/Problem 1. A+B/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problem 1. A+B
+//
+//  Created by Kieran Will on 1/3/16.
+//  Copyright © 2016 Kieran Will. All rights reserved.
+//
+
+#include <iostream>
+int a, b;
+int main(int argc, const char * argv[]) {
+    std::cin >> a >> b;
+    std::cout << a + b <<"\n";
+    return 0;
+}
